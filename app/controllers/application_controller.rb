de094ce8cd4e9d455642e30fb637b350c79c9874
FUMIGear <iso.fumi429@gmail.com>
2022-12-20 19:35:03 +0900

    Task11-2の模範回答通りにファイルを修正 疑問点の解消 Task11-3に行く前にPLを送る。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  #↑現在のユーザ情報を入手するため必要か？→ログインしてなければログイン画面に移るメソッド
 
   # protected
-
   # def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   # end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+	skip_before_action :authenticate_user!, only: %i[ index show ]
+  # before_action :set_article, only: %i[show edit update destroy ] #Task11-1まで
+  before_action :set_article, only: %i[ edit update destroy ] #Task11-2で変更
+
   # before_actionは何かを実行する前に行う処理
   # 下記にset_articleメソッドがある
   # onlyは処理を指定する。
   # %iはシンボル形式に変換する。だからonly:[:show,:edit,:update,:destroy]と書いてもok
+  # Task11-2:なぜbefore_actionからshowを消した？
 
-  before_action :authenticate_user! #現在のユーザ情報を入手するため必要か？
+
 
   #Task10で追加したコード
   # before_action :authenticate_user! #これいる？ヘルパー使う場合に使うbeforeaction→必要やった→やっぱ必要なかった
@@ -13,15 +17,18 @@ class ArticlesController < ApplicationController
   #上がないとトップ画面でログイン画面が出てこない
   #authenticate_memberがないとDBにユーザを登録できない。上のメソッドがない？
   # でもなくても行けた。よくわからないwwww
+  # 結論：authenticate_user!はログイン画面に移行するメソッド／アカウントの登録とは別。なくても動くが挙動は大きく変わるはず
 
   # GET /articles or /articles.json
   def index
     @articles = Article.all
-    #Article：データベース、.all：全部。データベース内のデータを全て@articleに入れる。
+    #Article：DBテーブル、.all：全部。つまり、Articleテーブルのデータを全て@articleに入れる。
   end
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id]) #追加
+    # Task11-2で追加になっているが、before_actionに組まないのはなぜか？
   end
 
   # GET /articles/new
@@ -35,21 +42,23 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
-    # binding.pry
-    @article.user_id = current_user.id
+    # @article = Article.new(article_params) #Task11-1まで
+    # @article.user_id = current_user.id #自分で変更したコード
+    @article = current_user.articles.new(article_params) # Task11-2で追加（模範）
 
-    respond_to do |format|
+    # respond_to do |format| #これなに？→レスポンスをフォーマットごとに変えてる。（HTML形式とJSON形式）
       if @article.save #記事が保存できるか
         # 真の場合の処理
-        format.html { redirect_to article_url(@article), notice: "記事の作成に成功しました！" }
+        redirect_to @article, notice: "#{t('activerecord.models.article')}を作成しました。" #Task11-2で模範回答に調整
+        # format.html { redirect_to article_url(@article), notice: "記事の作成に成功しました！" }
         # format.json { render :show, status: :created, location: @article }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity #模範回答
+        # format.html { render :new, status: :unprocessable_entity }
         # format.json { render json: @article.errors, status: :unprocessable_entity }
       end
-    end
-    # def create
+    # end
+    # def create #なぜこれを作ったのか覚えてない
     #   @article = Article.new(p
     #     title: params[:title],
     #     content: params[:content]
@@ -59,24 +68,24 @@ class ArticlesController < ApplicationController
     # end
   end
 
-  # PATCH/PUT /articles/1 or /articles/1.json
-
   def update
     # binding.pry
-    if  @article.user_id == current_user.id
-      respond_to do |format|
+    if  @article.user_id == current_user.id #アカウントが違った場合の処理
+      # respond_to do |format|
         if @article.update(article_params)
-          format.html { redirect_to article_url(@article), notice: "記事を更新しました。" }
+          redirect_to article_url(@article), notice: "記事を更新しました。"
+          # format.html { redirect_to article_url(@article), notice: "記事を更新しました。" }
           # format.json { render :show, status: :ok, location: @article } #jsonは使わない
           # redirect_to @article, notice: "Article was successfully updated." #模範回答
           # notice:"記事を更新しました"とサイト上に表示される機能
           # fromat.html{}はhtml形式に変更する。（.htmlを.jsonにするとjson形式になる）
         else
-          format.html { render :edit, status: :unprocessable_entity }
+          render :edit, status: :unprocessable_entity
+          # format.html { render :edit, status: :unprocessable_entity }
           # format.json { render json: @article.errors, status: :unprocessable_entity } #jsonは使わない
           # render :edit, status: :unprocessable_entity #模範回答
         end
-      end
+      # end
     else
       redirect_to article_url(@article), notice: "アカウントが違います！"
     end
@@ -99,13 +108,14 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      @article = current_user.articles.find(params[:id]) #Task11-2で追加、模範回答
+      # @article = Article.find(params[:id]) #Task11-1まで
       # before_actionの後ろにset_articleがあり、ここでその処理を行ってる。
       # "Article.find(id)"でArticlesテーブルの指定したidのレコードを引っ張る
       # .findは検索メソッド
       # DBとデータをやり取りする際にparamsメソッドを使う？
       # findメソッドを使わないとエラーになるのかな？
-      # params[:id]でエラーになったら、.find(params[:id])で試してみる
+      # params[:id]でエラーになったら、.find(params[:id])で試してみる→id参照するならfindメソッドは必要
     end
 
     # Only allow a list of trusted parameters through.
